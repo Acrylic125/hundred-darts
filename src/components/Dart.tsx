01460ab21d8480ea06d0ac34705703c1054ab684
Acrylic125 <acrylic125email@gmail.com>
2023-02-09 14:11:45 +0800

    feat: add duplicate and delete support

--- src/components/Dart.tsx
+++ src/components/Dart.tsx
@@ -1,70 +1,125 @@
 import { Box, InputBase, Typography } from "@mui/material";
 import { useRef, useState } from "react";
+import ContextMenu from "./ContextMenu";
 
 const Dart = ({
   content: _content,
   onRequestEdit,
+  onRequestDelete,
   onRequestSave,
+  onRequestDuplicate,
   onRequestClose,
   editMode,
   autoFocusOnEdit,
 }: {
   content: string;
   onRequestEdit?: () => void;
+  onRequestDelete?: () => void;
   onRequestSave?: (content: string) => void;
+  onRequestDuplicate?: () => void;
   onRequestClose?: () => void;
   editMode?: boolean;
   autoFocusOnEdit?: boolean;
 }) => {
+  const [contextMenu, setContextMenu] = useState<{
+    top: number;
+    left: number;
+  } | null>(null);
   const [content, setContent] = useState(_content);
   const editRef = useRef(null);
 
   return (
-    <Box
-      onClick={() => {
-        onRequestEdit?.();
-      }}
-      sx={{
-        backgroundColor: "grey.800",
-        padding: 2,
-        borderRadius: 2,
-        borderWidth: editMode ? 2 : 0,
-        borderStyle: "dashed",
-        borderColor: "primary.500",
-      }}
-    >
-      {editMode ? (
-        <InputBase
-          autoFocus={autoFocusOnEdit}
-          inputRef={editRef}
-          defaultValue={_content}
-          onChange={(e) => {
-            setContent(e.target.value);
-          }}
-          value={content}
-          onFocus={(e) =>
-            e.currentTarget.setSelectionRange(
-              e.currentTarget.value.length,
-              e.currentTarget.value.length
-            )
-          }
-          onBlur={() => {
-            onRequestClose?.();
-            onRequestSave?.(content);
-          }}
-          sx={{
-            width: "100%",
-            height: "100%",
-            color: "white",
-          }}
-          multiline
-        />
-      ) : (
-        <Typography variant="body1" component="p">
-          {content || "Empty Dart"}
-        </Typography>
-      )}
-    </Box>
+    <>
+      <ContextMenu
+        onRequestClose={() => {
+          setContextMenu(null);
+        }}
+        openState={contextMenu}
+        sections={[
+          {
+            items: [
+              {
+                label: "Duplicate",
+                shortcut: "Ctrl+D",
+                onClick: () => {
+                  onRequestDuplicate?.();
+                },
+              },
+              {
+                label: "Delete",
+                shortcut: "Delete",
+                onClick: () => {
+                  onRequestDelete?.();
+                },
+              },
+            ],
+          },
+        ]}
+      />
+      <Box
+        onContextMenu={(e) => {
+          e.preventDefault();
+          setContextMenu({
+            top: e.clientY,
+            left: e.clientX,
+          });
+        }}
+        onClick={() => {
+          onRequestEdit?.();
+        }}
+        sx={{
+          backgroundColor: "grey.800",
+          cursor: "context-menu",
+          padding: 2,
+          borderRadius: 2,
+          borderWidth: contextMenu || editMode ? 3 : 0,
+          borderStyle: "dashed",
+          borderColor: "primary.500",
+        }}
+      >
+        {editMode ? (
+          <InputBase
+            autoFocus={autoFocusOnEdit}
+            inputRef={editRef}
+            placeholder="Empty Dart"
+            defaultValue={_content}
+            onChange={(e) => {
+              setContent(e.target.value);
+            }}
+            value={content}
+            onFocus={(e) =>
+              e.currentTarget.setSelectionRange(
+                e.currentTarget.value.length,
+                e.currentTarget.value.length
+              )
+            }
+            onBlur={() => {
+              if (!editMode) return;
+              onRequestClose?.();
+              onRequestSave?.(content);
+            }}
+            sx={{
+              width: "100%",
+              height: "100%",
+              color: "white",
+            }}
+            multiline
+          />
+        ) : (
+          <Typography
+            sx={{
+              opacity: content ? 1 : 0.5,
+              whiteSpace: "pre-wrap",
+              wordWrap: "break-word",
+            }}
+            variant="body1"
+            component="p"
+          >
+            {content || "Empty Dart"}
+          </Typography>
+        )}
+      </Box>
+    </>
   );
 };
 

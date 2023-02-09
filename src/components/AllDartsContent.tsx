f1b454e8639d9772fbc05de71419333f28b205ac
Acrylic125 <acrylic125email@gmail.com>
2023-02-09 14:11:01 +0800

    feat: delete and duplicate dart

--- src/components/AllDartsContent.tsx
+++ src/components/AllDartsContent.tsx
@@ -11,12 +11,44 @@ import {
   Typography,
 } from "@mui/material";
 import type { Dart as DartType } from "@prisma/client";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Dart from "./Dart";
 
 const createDartSubscriber = new EventSubscriber<{
   content?: string;
 }>();
+
+type LocalId = string;
+
+function useLocalIdRemap() {
+  const [localIdRemap, setLocalIdRemap] = useState<Map<LocalId, string | null>>(
+    new Map()
+  );
+  const counter = useRef(0);
+
+  return {
+    newLocalId: (): LocalId => {
+      const newId = `local-${counter.current}`;
+      counter.current += 1;
+      setLocalIdRemap((prev) => {
+        const newMap = new Map(prev);
+        newMap.set(newId, null);
+        return newMap;
+      });
+      return newId;
+    },
+    bindLocalId: (localId: LocalId, remoteId: string) => {
+      setLocalIdRemap((prev) => {
+        const newMap = new Map(prev);
+        newMap.set(localId, remoteId);
+        return newMap;
+      });
+    },
+    getMappedId: (localId: LocalId) => {
+      return localIdRemap.get(localId);
+    },
+  };
+}
 
 const AllDartsExtras = () => {
   return (
@@ -54,11 +86,39 @@ const AllDartsExtras = () => {
 const AllDartsContent = ({ dartBoardId }: { dartBoardId: string }) => {
   const [edittedDartId, setEdittedDartId] = useState<string | null>(null);
   const [darts, setDarts] = useState<DartType[] | null>(null);
+  const { newLocalId, bindLocalId, getMappedId } = useLocalIdRemap();
   useEventSubscriber(createDartSubscriber, ({ content }) => {
-    void createDart({
-      dartBoardId,
-      text: content ?? "",
+    const localId = newLocalId();
+    setEdittedDartId(localId);
+    setDarts((prev) => {
+      if (prev === null) {
+        return null;
+      }
+      return [
+        ...prev,
+        {
+          id: localId,
+          text: content ?? "",
+          dartBoardId,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          deletedAt: null,
+        },
+      ];
     });
+    void createDart(
+      {
+        dartBoardId,
+        text: content ?? "",
+      },
+      {
+        onSuccess: (data) => {
+          if (data !== null) {
+            bindLocalId(localId, data.id);
+          }
+        },
+      }
+    );
   });
   const { isLoading: dartsIsLoading } = api.dart.getAllDartsForBoard.useQuery(
     {
@@ -71,6 +131,8 @@ const AllDartsContent = ({ dartBoardId }: { dartBoardId: string }) => {
     }
   );
   const { mutateAsync: createDart } = api.dart.createDart.useMutation();
+  const { mutateAsync: updateDart } = api.dart.updateDart.useMutation();
+  const { mutateAsync: deleteDart } = api.dart.deleteDart.useMutation();
 
   return (
     <Grid spacing={2} container>
@@ -81,21 +143,95 @@ const AllDartsContent = ({ dartBoardId }: { dartBoardId: string }) => {
           </Grid>
         </>
       ) : (
-        darts?.map((dart) => (
-          <Grid key={dart.id} item xs={12} sm={6} lg={4}>
-            <Dart
-              content={dart.text}
-              onRequestEdit={() => {
-                setEdittedDartId(dart.id);
-              }}
-              onRequestClose={() => {
-                setEdittedDartId(null);
-              }}
-              editMode={edittedDartId === dart.id}
-              autoFocusOnEdit
-            />
-          </Grid>
-        ))
+        darts
+          ?.sort((a, b) => {
+            return b.createdAt.getTime() - a.createdAt.getTime();
+          })
+          .map((dart) => (
+            <Grid key={dart.id} item xs={12} sm={6} lg={4}>
+              <Dart
+                content={dart.text}
+                onRequestEdit={() => {
+                  setEdittedDartId(dart.id);
+                }}
+                onRequestDelete={() => {
+                  setDarts((prev) => {
+                    if (prev === null) {
+                      return null;
+                    }
+                    return prev.filter((d) => d.id !== dart.id);
+                  });
+                  void deleteDart(
+                    {
+                      dartId: getMappedId(dart.id) ?? dart.id,
+                    },
+                    {
+                      onError: () => {
+                        setDarts((prev) => {
+                          if (prev === null) {
+                            return null;
+                          }
+                          return [...prev, dart];
+                        });
+                      },
+                    }
+                  );
+                }}
+                onRequestDuplicate={() => {
+                  const localId = newLocalId();
+                  setEdittedDartId(null);
+                  setDarts((prev) => {
+                    if (prev === null) {
+                      return null;
+                    }
+                    return [
+                      ...prev,
+                      {
+                        id: localId,
+                        text: dart.text + " duplicated",
+                        dartBoardId,
+                        createdAt: new Date(),
+                        updatedAt: new Date(),
+                        deletedAt: null,
+                      },
+                    ];
+                  });
+                  void createDart(
+                    {
+                      dartBoardId,
+                      text: dart.text,
+                    },
+                    {
+                      onSuccess: (data) => {
+                        if (data !== null) {
+                          bindLocalId(localId, data.id);
+                        }
+                      },
+                      onError: () => {
+                        setDarts((prev) => {
+                          if (prev === null) {
+                            return null;
+                          }
+                          return prev.filter((d) => d.id !== localId);
+                        });
+                      },
+                    }
+                  );
+                }}
+                onRequestClose={() => {
+                  setEdittedDartId(null);
+                }}
+                onRequestSave={(content) => {
+                  void updateDart({
+                    dartId: getMappedId(dart.id) ?? dart.id,
+                    text: content,
+                  });
+                }}
+                editMode={edittedDartId === dart.id}
+                autoFocusOnEdit
+              />
+            </Grid>
+          ))
       )}
     </Grid>
   );

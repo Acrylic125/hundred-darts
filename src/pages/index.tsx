461556403bafb6b8434b3473195b3c247a900d50
Acrylic125 <acrylic125email@gmail.com>
2023-03-14 10:35:08 +0800

    testing out slate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,79 @@
 import { type NextPage } from "next";
 import { useSession } from "next-auth/react";
-
 import { api } from "../utils/api";
+import { Editable, Slate, withReact } from "slate-react";
+
+import type { Descendant, Editor } from "slate";
+import { createEditor } from "slate";
+import { useMemo } from "react";
+import { DefaultElement } from "slate-react";
+
+function useEditorConfig(editor: Editor) {
+  return { renderElement, renderLeaf };
+}
+
+function renderElement(props) {
+  const { element, children, attributes } = props;
+  switch (element.type) {
+    case "paragraph":
+      return <p {...attributes}>{children}</p>;
+    case "h1":
+      return <h1 {...attributes}>{children}</h1>;
+    case "h2":
+      return <h2 {...attributes}>{children}</h2>;
+    case "h3":
+      return <h3 {...attributes}>{children}</h3>;
+    case "h4":
+      return <h4 {...attributes}>{children}</h4>;
+    default:
+      // For the default case, we delegate to Slate's default rendering.
+      return <DefaultElement {...props} />;
+  }
+}
+
+function renderLeaf({ attributes, children, leaf }) {
+  let el = <>{children}</>;
+
+  if (leaf.bold) {
+    el = <strong>{el}</strong>;
+  }
+
+  if (leaf.code) {
+    el = <code>{el}</code>;
+  }
+
+  if (leaf.italic) {
+    el = <em>{el}</em>;
+  }
+
+  if (leaf.underline) {
+    el = <u>{el}</u>;
+  }
+
+  return <span {...attributes}>{el}</span>;
+}
+
+const ExampleDocument = [
+  {
+    type: "paragraph",
+    children: [
+      { text: "Hello World! This is my paragraph inside a sample document." },
+      { text: "Bold text.", bold: true, code: true },
+      { text: "Italic text.", italic: true },
+      { text: "Bold and underlined text.", bold: true, underline: true },
+      { text: "variableFoo", code: true },
+    ],
+  },
+];
 
 const Home: NextPage = () => {
-  const { data, status } = useSession();
-
-  return <></>;
+  const editor = useMemo(() => withReact(createEditor()), []);
+  const { renderElement, renderLeaf } = useEditorConfig(editor);
+  return (
+    <Slate editor={editor} value={ExampleDocument}>
+      <Editable renderElement={renderElement} renderLeaf={renderLeaf} />
+    </Slate>
+  );
 };
 
 export default Home;
